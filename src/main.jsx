0bbb1271d578f59dcccaa62cfafd4253a474f0d5
babji <2300039117@kluniversity.in>
2025-07-28 20:12:20 +0530

    mdified

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import Dashboard from './Dashboard.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<App></App>}></Route>
-      <Route path='dashboard' element={<Dashboard/>}></Route>
-  </Routes>
+    <Routes>
+      <Route path='/jobportal' element={<App />}></Route>
+      <Route path='/jobportal/dashboard' element={<Dashboard />}></Route>
+    </Routes>
   </BrowserRouter>
 )
